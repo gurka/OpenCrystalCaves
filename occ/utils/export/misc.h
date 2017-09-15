63af8e81459f76b0dc291293a812fa2a47e774c5
Simon Sandström <simon@nikanor.nu>
2017-09-15 20:37:45 +0200

    Add misc::random<T>

--- occ/utils/export/misc.h
+++ occ/utils/export/misc.h
@@ -2,6 +2,7 @@
 #define MISC_H_
 
 #include <array>
+#include <random>
 #include <type_traits>
 #include <utility>
 
@@ -12,6 +13,15 @@ template <typename... T>
 constexpr auto make_array(T&&... values) -> std::array<typename std::decay<typename std::common_type<T...>::type>::type, sizeof...(T)>
 {
   return std::array<typename std::decay<typename std::common_type<T...>::type>::type, sizeof...(T)>{std::forward<T>(values)...};
+}
+
+template<typename T>
+T random(T min, T max)
+{
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  std::uniform_int_distribution<T> dis(min, max);
+  return dis(gen);
 }
 
 }
