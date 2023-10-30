1247e73717d2839ac6c14958738c92e3ef12f52b
Cong <congusbongus@gmail.com>
2023-10-30 23:28:39 +1100

    Read sounds into structures #11

--- occ/sound_player/sound_player.cc
+++ occ/sound_player/sound_player.cc
@@ -12,6 +12,15 @@ https://moddingwiki.shikadi.net/wiki/Crystal_Caves_Sound_format
 #define GOG_ID "1207665273"
 #define GAME_NAME "Crystal Caves"
 
+struct Sound
+{
+	uint16_t priority;
+	uint16_t unknown;
+	uint16_t vibrate;
+	uint32_t unknown2;
+	int16_t data[300];
+};
+
 std::filesystem::path get_sound_path(const int idx)
 {
 	// TODO: use <format> in C++20
@@ -48,6 +57,7 @@ std::filesystem::path get_sound_path(const int idx)
 
 int main()
 {
+	std::vector<Sound> sounds;
 	for (int i = 0; ; i++)
 	{
 		const auto path = get_sound_path(i);
@@ -55,8 +65,16 @@ int main()
 		{
 			break;
 		}
-		std::cout << "Found sound file at " << path << "\n";
+		std::cout << "Reading sound file at " << path << "\n";
+		std::ifstream input{path, std::ios::binary};
+		Sound sound;
+		while (input.read(reinterpret_cast<char*>(&sound), sizeof sound))
+		{
+			std::cout << +sound.priority << "," << +sound.vibrate << "," << +sound.unknown << "," << +sound.unknown2 << "\n";
+			sounds.push_back(sound);
+		}
 	}
+	std::cout << "Read " << sounds.size() << " sounds\n";
 
   return 0;
 }
