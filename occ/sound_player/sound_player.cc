aaeafe90f41d41ca7158c9c3032269a8f87d3c67
Cong <congusbongus@gmail.com>
2023-12-04 22:21:22 +1100

    Update sound_player.cc

--- occ/sound_player/sound_player.cc
+++ occ/sound_player/sound_player.cc
@@ -8,6 +8,7 @@ https://moddingwiki.shikadi.net/wiki/Crystal_Caves_Sound_format
 #include <vector>
 
 #include <find_steam_game.h>
+#define SDL_MAIN_HANDLED
 #include <SDL.h>
 
 #define SND_FILENAME_FMT "CC1-{}.SND"
@@ -20,18 +21,19 @@ https://moddingwiki.shikadi.net/wiki/Crystal_Caves_Sound_format
 
 struct Sound
 {
-	uint16_t priority;
-	uint16_t unknown0;
-	uint16_t vibrate;
-	uint16_t unknown1;
-	uint16_t unknown2;
-	int16_t data[300];
+  uint16_t priority;
+  uint16_t unknown0;
+  uint16_t vibrate;
+  uint16_t unknown1;
+  uint16_t unknown2;
+  int16_t data[300];
 };
 
 std::filesystem::path get_sound_path(const int idx)
 {
-	// TODO: use <format> in C++20
-	const auto filename = std::string(SND_FILENAME_FMT).replace(std::string(SND_FILENAME_FMT).find("{}"), std::string("{}").length(), std::to_string(idx + 1));
+  // TODO: use <format> in C++20
+  const auto filename =
+    std::string(SND_FILENAME_FMT).replace(std::string(SND_FILENAME_FMT).find("{}"), std::string("{}").length(), std::to_string(idx + 1));
   // Try CWD first
   if (std::filesystem::exists(filename))
   {
@@ -48,235 +50,228 @@ std::filesystem::path get_sound_path(const int idx)
   );
   if (buf[0])
   {
-	  const auto gogfilename = std::filesystem::path(buf)
+    const auto gogfilename = std::filesystem::path(buf)
 #ifdef __APPLE__
       / "game"
 #endif
       / filename;
-	  if (std::filesystem::exists(gogfilename))
-	  {
-		return std::filesystem::path(gogfilename);
-	  }
+    if (std::filesystem::exists(gogfilename))
+    {
+      return std::filesystem::path(gogfilename);
+    }
   }
   // TODO: Try steam
   return std::filesystem::path();
 }
 
-struct SoundData {
-	SDL_AudioSpec spec;
-	std::vector<Sound> sounds;
-	int sound_index = -1;
-	int counter = 0;
-	int freq_counter = 0;
-	int index = -1;
-	int dc = 64;
+struct SoundData
+{
+  SDL_AudioSpec spec;
+  std::vector<Sound> sounds;
+  int sound_index = -1;
+  int counter = 0;
+  int freq_counter = 0;
+  int index = -1;
+  int dc = 64;
 
-	// Bytes per sample (single channel)
-	int bps() const
-	{
-		return (spec.format & 0xFF) / 8;
-	}
+  // Bytes per sample (single channel)
+  int bps() const { return (spec.format & 0xFF) / 8; }
 
-	const Sound& sound() const
-	{
-		return sounds[sound_index];
-	}
-	
-	int16_t freq() const
-	{
-		return sound().data[index];
-	}
+  const Sound& sound() const { return sounds[sound_index]; }
 
-	int amp() const
-	{
-		if (freq() == 0 || (index % sound().vibrate) == 0)
-		{
-			return spec.silence;
-		}
-		return dc;
-	}
-	
-	void tick()
-	{
-		if (counter == 0)
-		{
-			nextfreq();
-		}
-		else
-		{
-			if (freq_counter == 0)
-			{
-				freq_counter = sound().data[index];
-				dc *= -1;
-			}
-			counter--;
-			freq_counter--;
-		}
-	}
+  int16_t freq() const { return sound().data[index]; }
 
-	void nextfreq()
-	{
-		if (is_end())
-		{
-			return;
-		}
-		index++;
-		freq_counter = 0;
-		counter = 256;
-	}
+  int amp() const
+  {
+    if (freq() == 0 || (index % sound().vibrate) == 0)
+    {
+      return spec.silence;
+    }
+    return dc;
+  }
 
-	bool is_end() const
-	{
-		return sound_index == -1 || index >= 300 || counter == -1 || freq() == -1;
-	}
+  void tick()
+  {
+    if (counter == 0)
+    {
+      nextfreq();
+    }
+    else
+    {
+      if (freq_counter == 0)
+      {
+        freq_counter = sound().data[index];
+        dc *= -1;
+      }
+      counter--;
+      freq_counter--;
+    }
+  }
 
-	void play(const int _sound_index)
-	{
-		sound_index = _sound_index;
-		std::cout << "Playing sound " << sound_index << " vibrate=" << std::hex << sound().vibrate << " unknown0=" << sound().unknown0 << " unknown1=" << sound().unknown1 << " unknown2=" << sound().unknown2 << std::dec << "\n";
-		index = -1;
-		freq_counter = 0;
-		counter = 256;
-	}
+  void nextfreq()
+  {
+    if (is_end())
+    {
+      return;
+    }
+    index++;
+    freq_counter = 0;
+    counter = 256;
+  }
+
+  bool is_end() const { return sound_index == -1 || index >= 300 || counter == -1 || freq() == -1; }
+
+  void play(const int _sound_index)
+  {
+    sound_index = _sound_index;
+    std::cout << "Playing sound " << sound_index << " vibrate=" << std::hex << sound().vibrate << " unknown0=" << sound().unknown0
+              << " unknown1=" << sound().unknown1 << " unknown2=" << sound().unknown2 << std::dec << "\n";
+    index = -1;
+    freq_counter = 0;
+    counter = 256;
+  }
 };
 
 void callback(void* userdata, Uint8* stream, int len)
 {
-	SoundData* sdata = reinterpret_cast<SoundData*>(userdata);
-	int i = 0;
-	do
-	{
-		sdata->tick();
-		// Generate square wave at frequency
-		for (int c = 0; c < sdata->spec.channels; c++)
-		{
-			for (int b = 0; b < sdata->bps() && i < len; b++, i++)
-			{
-				stream[i] = sdata->amp();
-			}
-		}
-	}
-	while (i < len);
+  SoundData* sdata = reinterpret_cast<SoundData*>(userdata);
+  int i = 0;
+  do
+  {
+    if (sdata->index == -1)
+    {
+      memset(stream, 0, len);
+    }
+    else
+    {
+      sdata->tick();
+      // Generate square wave at frequency
+      for (int c = 0; c < sdata->spec.channels; c++)
+      {
+        for (int b = 0; b < sdata->bps() && i < len; b++, i++)
+        {
+          stream[i] = (Uint8)sdata->amp();
+        }
+      }
+    }
+  } while (i < len);
 }
 
 char itoc(int i)
 {
-	if (i < 10)
-	{
-		return (char)('0' + i);
-	}
-	i -= 10;
-	if (i < 26)
-	{
-		return (char)('a' + i);
-	}
-	i -= 26;
-	if (i < 26)
-	{
-		return (char)('A' + i);
-	}
-	return '?';
+  if (i < 10)
+  {
+    return (char)('0' + i);
+  }
+  i -= 10;
+  if (i < 26)
+  {
+    return (char)('a' + i);
+  }
+  i -= 26;
+  if (i < 26)
+  {
+    return (char)('A' + i);
+  }
+  return '?';
 }
 int keytoi(const SDL_Keycode c, const bool shift)
 {
-	int d = 0;
-	if (c >= SDLK_0 && c <= SDLK_9)
-	{
-		return c - SDLK_0 + d;
-	}
-	d += 10;
-	if (shift)
-	{
-		d += 26;
-	}
-	if (c >= SDLK_a && c <= SDLK_z)
-	{
-		return c - SDLK_a + d;
-	}
-	return -1;
+  int d = 0;
+  if (c >= SDLK_0 && c <= SDLK_9)
+  {
+    return c - SDLK_0 + d;
+  }
+  d += 10;
+  if (shift)
+  {
+    d += 26;
+  }
+  if (c >= SDLK_a && c <= SDLK_z)
+  {
+    return c - SDLK_a + d;
+  }
+  return -1;
 }
 
 int main()
 {
-	SoundData sdata;
-	for (int i = 0; ; i++)
-	{
-		const auto path = get_sound_path(i);
-		if (path.empty())
-		{
-			break;
-		}
-		std::cout << "Reading sound file at " << path << "\n";
-		std::ifstream input{path, std::ios::binary};
-		Sound sound;
-		while (input.read(reinterpret_cast<char*>(&sound), sizeof sound))
-		{
-			sdata.sounds.push_back(sound);
-		}
-	}
-	std::cout << "Read " << sdata.sounds.size() << " sounds\n";
-	
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
-	{
-		std::cout << "Could not initialize SDL: " << SDL_GetError() << "\n";
-		return 1;
-	}
+  SoundData sdata;
+  for (int i = 0;; i++)
+  {
+    const auto path = get_sound_path(i);
+    if (path.empty())
+    {
+      break;
+    }
+    std::cout << "Reading sound file at " << path << "\n";
+    std::ifstream input{path, std::ios::binary};
+    Sound sound;
+    while (input.read(reinterpret_cast<char*>(&sound), sizeof sound))
+    {
+      sdata.sounds.push_back(sound);
+    }
+  }
+  std::cout << "Read " << sdata.sounds.size() << " sounds\n";
 
-	SDL_AudioSpec want;
-	want.freq = SAMPLE_RATE;
-	want.format = AUDIO_FMT;
-	want.channels = AUDIO_CHANNELS;
-	want.samples = 4096;
-	want.callback = callback;
-	want.userdata = &sdata;
-	const auto device = SDL_OpenAudioDevice(nullptr,
-		   0,
-		   &want,
-		   &sdata.spec, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
-	if (device == 0)
-	{
-		std::cout << "Could open audio: " << SDL_GetError() << "\n";
-		return 1;
-	}
-	
-	SDL_PauseAudioDevice(device, 0);
-	
-	std::cout << "Play sound by pressing 0 - " << itoc((int)sdata.sounds.size()) << "\n";
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+  {
+    std::cout << "Could not initialize SDL: " << SDL_GetError() << "\n";
+    return 1;
+  }
 
-	bool quit = false;
-	bool keydown = false;
-	while (!quit)
-	{
-		SDL_Event event;
-		while (SDL_PollEvent(&event) != 0)
-		{
-			if (event.type == SDL_QUIT)
-			{
-				quit = true;
-			}
-			else if (!keydown && event.type == SDL_KEYDOWN)
-			{
-				if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
-				{
-					quit = true;
-				}
-				keydown = true;
-				const bool shift = !!(SDL_GetModState() & KMOD_SHIFT);
-				const int index = keytoi(event.key.keysym.sym, shift);
-				if (index >= 0 && index < (int)sdata.sounds.size())
-				{
-					sdata.play(index);
-				}
-			}
-			else if (event.type == SDL_KEYUP)
-			{
-				keydown = false;
-			}
-		}
-		SDL_Delay(10);
-	}
-	
-	SDL_CloseAudioDevice(device);
+  SDL_AudioSpec want;
+  want.freq = SAMPLE_RATE;
+  want.format = AUDIO_FMT;
+  want.channels = AUDIO_CHANNELS;
+  want.samples = 4096;
+  want.callback = callback;
+  want.userdata = &sdata;
+  const auto device = SDL_OpenAudioDevice(nullptr, 0, &want, &sdata.spec, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
+  if (device == 0)
+  {
+    std::cout << "Could open audio: " << SDL_GetError() << "\n";
+    return 1;
+  }
+
+  SDL_PauseAudioDevice(device, 0);
+
+  std::cout << "Play sound by pressing 0 - " << itoc((int)sdata.sounds.size()) << "\n";
+
+  bool quit = false;
+  bool keydown = false;
+  while (!quit)
+  {
+    SDL_Event event;
+    while (SDL_PollEvent(&event) != 0)
+    {
+      if (event.type == SDL_QUIT)
+      {
+        quit = true;
+      }
+      else if (!keydown && event.type == SDL_KEYDOWN)
+      {
+        if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+        {
+          quit = true;
+        }
+        keydown = true;
+        const bool shift = !!(SDL_GetModState() & KMOD_SHIFT);
+        const int index = keytoi(event.key.keysym.sym, shift);
+        if (index >= 0 && index < (int)sdata.sounds.size())
+        {
+          sdata.play(index);
+        }
+      }
+      else if (event.type == SDL_KEYUP)
+      {
+        keydown = false;
+      }
+    }
+    SDL_Delay(10);
+  }
+
+  SDL_CloseAudioDevice(device);
 
   return 0;
 }
