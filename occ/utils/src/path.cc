8e2d18e31f2dea690d1689d005d92539138d14d4
Cong <congusbongus@gmail.com>
2024-01-03 23:29:06 +1100

    Support steam windows #11

--- occ/utils/src/path.cc
+++ occ/utils/src/path.cc
@@ -7,32 +7,43 @@
 
 std::filesystem::path get_data_path(const std::filesystem::path& filename)
 {
-	// Try CWD first
-	if (std::filesystem::exists(filename))
-	{
-		return std::filesystem::path(filename);
-	}
-	// Try GoG
-	char buf[4096];
-	fsg_get_gog_game_path(buf,
-  #ifdef _WIN32
-						  GOG_ID
-  #else
-						  GAME_NAME
-  #endif
-	);
-	if (buf[0])
-	{
-		const auto gogfilename = std::filesystem::path(buf)
-  #ifdef __APPLE__
-		/ "game"
-  #endif
-		/ filename;
-		if (std::filesystem::exists(gogfilename))
-		{
-		  return std::filesystem::path(gogfilename);
-		}
-	}
-	// TODO: Try steam
-	return std::filesystem::path();
+  // Try CWD first
+  if (std::filesystem::exists(filename))
+  {
+    return std::filesystem::path(filename);
+  }
+  char buf[4096];
+  // Try steam
+  fsg_get_steam_game_path(buf, GAME_NAME);
+  if (buf[0])
+  {
+    const auto steamfilename = std::filesystem::path(buf) / GAME_NAME / filename;
+    if (std::filesystem::exists(steamfilename))
+    {
+      return std::filesystem::path(steamfilename);
+    }
+    // TODO: apple
+  }
+  // Try GoG
+  fsg_get_gog_game_path(buf,
+#ifdef _WIN32
+                        GOG_ID
+#else
+                        GAME_NAME
+#endif
+  );
+  if (buf[0])
+  {
+    const auto gogfilename = std::filesystem::path(buf)
+#ifdef __APPLE__
+      / "game"
+#endif
+      / filename;
+    if (std::filesystem::exists(gogfilename))
+    {
+      return std::filesystem::path(gogfilename);
+    }
+  }
+  // TODO: Try steam
+  return std::filesystem::path();
 }
